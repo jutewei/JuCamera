6e1cb4efe0a42166456ff9dc17d8b1a94e32e500
juvid <jutewei@qq.com>
2017-10-18 16:43:24 +0800

    Initial Commit

---
+++ JuCamera/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JuCamera
+//
+//  Created by Juvid on 2017/10/18.
+//  Copyright © 2017年 Juvid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
