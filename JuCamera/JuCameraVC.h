d81b6c1ff2868971084ac3c875eb2a0357bc64a8
juvid <jutewei@qq.com>
2017-10-19 16:15:52 +0800

    first commit

---
+++ JuCamera/JuCameraVC.h
@@ -0,0 +1,13 @@
+//
+//  JuCameraVC.h
+//  JuCamera
+//
+//  Created by Juvid on 2017/10/19.
+//  Copyright © 2017年 Juvid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JuCameraVC : UIViewController
+
+@end
---
+++ JuCamera/JuCameraVC.m
@@ -0,0 +1,37 @@
+//
+//  JuCameraVC.m
+//  JuCamera
+//
+//  Created by Juvid on 2017/10/19.
+//  Copyright © 2017年 Juvid. All rights reserved.
+//
+
+#import "JuCameraVC.h"
+
+@interface JuCameraVC ()
+
+@end
+
+@implementation JuCameraVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
