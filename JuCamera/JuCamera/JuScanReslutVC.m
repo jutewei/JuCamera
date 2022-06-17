d036c419ab609301dcd8bde35a8cfc022b88aefc
Juvid <jutewei@qq.com>
2022-06-17 15:52:49 +0800

    UI 修改

--- JuCamera/JuCamera/JuScanReslutVC.m
+++ JuCamera/JuCamera/JuScanReslutVC.m
@@ -8,10 +8,12 @@
 
 #import "JuScanReslutVC.h"
 #import "JuWebVC.h"
+#import "MBProgressHUD+Share.h"
 
 @interface JuScanReslutVC ()
 @property (nonatomic,weak) IBOutlet UITextView *ju_textView;
 @property (nonatomic,weak) IBOutlet UIButton *ju_btnNext;
+@property (nonatomic,weak) IBOutlet UIButton *ju_btnCopy;
 
 @end
 
@@ -19,19 +21,36 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    _ju_textView.textContainerInset=UIEdgeInsetsMake(5, 5, 5, 5);
-    [_ju_textView.layer setCornerRadius:4];
-    [_ju_textView.layer setMasksToBounds:YES];
+    
+    self.view.backgroundColor=[UIColor colorWithWhite:0.98 alpha:1];
+
+//    _ju_textView.textContainerInset=UIEdgeInsetsMake(5, 5, 5, 5);
+    [_ju_textView.superview.layer setCornerRadius:10];
+    [_ju_textView.superview.layer setMasksToBounds:YES];
+    
+    
+    [_ju_btnCopy.layer setCornerRadius:10];
+    [_ju_btnCopy.layer setMasksToBounds:YES];
+    
+    
+    [_ju_btnNext.layer setCornerRadius:10];
+    [_ju_btnNext.layer setMasksToBounds:YES];
+//    [_ju_textView.layer setCornerRadius:4];
+//    [_ju_textView.layer setMasksToBounds:YES];
     
     self.title = @"扫描结果";
     _ju_textView.text=_ju_result;
     NSURL *url=[NSURL URLWithString:_ju_result];
-    if (url.scheme.length) {
-        self.ju_btnNext.hidden = NO;
+    if (url.scheme.length==0) {
+        [self.ju_btnNext removeFromSuperview];
     }
     // Do any additional setup after loading the view.
 }
-
+- (IBAction)juTouchCopy:(id)sender{
+    UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
+    pasteboard.string=_ju_result;
+    [MBProgressHUD juShowHUDText:@"复制成功"];
+}
 - (IBAction)juTouchNext:(id)sender{
     NSURL *url=[NSURL URLWithString:_ju_result];
     if ([url.scheme hasPrefix:@"http"]) {
--- JuCamera/JuCamera/ViewController.m
+++ JuCamera/JuCamera/ViewController.m
@@ -15,6 +15,7 @@
 #import "MBProgressHUD+Share.h"
 #import "JuScanReslutVC.h"
 #import "PABaseNavigationC.h"
+#import "UIImage+drawImage.h"
 
 //#import <AVFoundation/AVFoundation.h>
 @interface ViewController ()<UITextViewDelegate>{
@@ -23,6 +24,7 @@
 @property (weak, nonatomic) IBOutlet UIImageView *juImageView;
 @property (weak, nonatomic) IBOutlet UITextView *juTextView;
 @property (weak, nonatomic) IBOutlet UILabel *ju_labHint;
+@property (weak, nonatomic) IBOutlet UIButton *ju_btnScan;
 
 //@property (nonatomic, strong) AVAudioPlayer *player;
 @end
@@ -31,19 +33,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.title = @"二维码";
-    _juTextView.textContainerInset=UIEdgeInsetsMake(10, 8, 10, 8);
-    [_juTextView.layer setCornerRadius:4];
-    [_juTextView.layer setMasksToBounds:YES];
-    [_juImageView.layer setCornerRadius:4];
-    [_juImageView.layer setMasksToBounds:YES];
     
-    [self setBarRightItem:@"扫一扫"];
-    [self shSetNew:2|1|3];
+    self.view.backgroundColor=[UIColor colorWithWhite:0.98 alpha:1];
+    self.title = @"二维码";
+    
+    [self juSetView];
+//    [self shSetNew:2|1|3];
     // Do any additional setup after loading the view, typically from a nib.
 }
+-(void)juSetView{
+    _juTextView.textContainerInset=UIEdgeInsetsMake(5, 5, 5, 5);
+    [_juTextView.superview.layer setCornerRadius:10];
+    [_juTextView.superview.layer setMasksToBounds:YES];
+    
+//    [_juImageView.superview.layer setCornerRadius:5];
+//    [_juImageView.superview.layer setMasksToBounds:YES];
+    
+    _juImageView.superview.backgroundColor=[UIColor whiteColor];
+    
+    [_ju_btnScan.layer setCornerRadius:5];
+    [_ju_btnScan.layer setMasksToBounds:YES];
 
--(void)zlTouchRightItems:(UIButton *)sender{
+    [_ju_btnScan setBackgroundImage:[UIImage juImageWithColor:[UIColor redColor]] forState:UIControlStateNormal];
+}
+
+- (IBAction)juTouchScan:(UIButton *)sender{
     JuScanVC *vc=[[JuScanVC alloc]init];
     PABaseNavigationC *nav=[PABaseNavigationC zlBasicNation:vc];
     vc.ju_handle = ^(id  _Nullable result) {
@@ -54,29 +68,33 @@
     nav.modalPresentationStyle=0;
     [self presentViewController:nav animated:YES completion:nil];
 }
-
 -(void)textViewDidChange:(UITextView *)textView{
     _ju_labHint.hidden=textView.text.length;
 }
 
--(BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text{
-    if ([text isEqual:@"\n"]) {
-        [textView resignFirstResponder];
-        _juImageView.image=[UIImage juCreatQR:textView.text size:260];
-        return NO;
-    }
-    return YES;
-}
+//-(BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text{
+//    if ([text isEqual:@"\n"]) {
+//        [textView resignFirstResponder];
+//        _juImageView.image=[UIImage juCreatQR:textView.text size:260];
+//        return NO;
+//    }
+//    return YES;
+//}
 
-- (IBAction)juTouchSave:(id)sender{
+- (IBAction)juTouchSave:(UIButton *)sender{
 
-    UIImage *image=_juImageView.image;
-    if (image) {
-        [image juSaveRHAssetPhoto:^(PHAsset *asset) {
-            if (asset) {
-                [MBProgressHUD juShowHUDText:@"保存成功"];
-            }
-        }];
+    if(sender.tag==1){
+        _juImageView.image=[UIImage juCreatQR:_juTextView.text size:341];
+    }else{
+        
+        if (_juImageView.image) {
+            UIImage *image=[UIImage screenView:_juImageView.superview];
+            [image juSaveRHAssetPhoto:^(PHAsset *asset) {
+                if (asset) {
+                    [MBProgressHUD juShowHUDText:@"保存成功"];
+                }
+            }];
+        }
     }
 }
 
